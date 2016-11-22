f2fd2b93f65871dbab1e4faa436c171c47b6fde3
Keith Barrow <Keith.Barrow@tombola.com>
2016-11-22 17:37:28 +0000

    Scaled the stage, not responsive.

--- site/scripts/initial.js
+++ site/scripts/initial.js
@@ -1,3 +1,22 @@
-/**
- * Created by keith.barrow on 22/11/2016.
- */
+var STAGE_WIDTH = 512,
+    STAGE_HEIGHT = 192,
+    renderer = PIXI.autoDetectRenderer(STAGE_WIDTH, STAGE_HEIGHT, {
+        antialising: true,
+        transparent: false,
+        resolution: window.devicePixelRatio,
+        autoResize: true
+    }),
+    stage = new PIXI.Container();
+
+document.body.appendChild(renderer.view);
+renderer.render(stage);
+
+function resizeStage(){
+    var ratio = Math.min(window.innerHeight / STAGE_HEIGHT, window.innerWidth / STAGE_WIDTH);
+
+    stage.scale.x = stage.scale.y = ratio;
+    renderer.resize(Math.ceil(STAGE_WIDTH  * ratio), Math.ceil(STAGE_HEIGHT * ratio));
+}
+
+resizeStage();
+
